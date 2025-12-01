ce3000a1faf68d5e0eb4acfeaaa884f5c2684aa6
marta <martaescribanorguez@gmail.com>
2025-12-01 19:38:46 +0100

    cambios

--- board.py
+++ board.py
@@ -126,4 +126,4 @@ class Board:
 
             # Text in screen without having to do the letters
             pyxel.text(40,5, "Easy", 15)
-            pyxel.text(244, 7, str(self.score), 0)qa
+            pyxel.text(244, 7, str(self.score), 0)
