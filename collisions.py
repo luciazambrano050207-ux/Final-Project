2ed74601f14704c3d563e5bcf1df45abd853d50c
marta <martaescribanorguez@gmail.com>
2025-12-07 18:23:34 +0100

    cambios

--- collisions.py
+++ collisions.py
@@ -1,20 +1,19 @@
 class Collisions:
     """ This class represents the collisions between two objects. """
 
-    def __init__(self):
-        pass
-
-    def collide(self, a, b):
+    @staticmethod
+    def collide(a, b):
         return (a.x < b.x + b.width and
                 a.x + a.width > b.x and
                 a.y < b.y + b.height and
                 a.y + a.height > b.y)
 
-    def collision(self, packages, mario, luigi, boss, truck, score):
+    @staticmethod
+    def collision(packages, mario, luigi, boss, truck, score):
         for pkg in packages:
             if not pkg.fall and not pkg.at_truck:
                 if pkg.belt == 0 and 180 <= pkg.x <= 186:
-                    if self.collide(pkg, mario):
+                    if Collisions.collide(pkg, mario):
                         pkg.put_belt(0, 156, 82)
                         score.pkg_delivered()
                     else:
@@ -22,7 +21,7 @@ class Collisions:
                         mario.fall_package()
                         boss.fall_mario()
                 elif pkg.belt == 0 and pkg.x <= 80:
-                    if self.collide(pkg, luigi):
+                    if Collisions.collide(pkg, luigi):
                         pkg.put_belt(1, 84,66)
                         score.pkg_delivered()
                     else:
@@ -30,7 +29,7 @@ class Collisions:
                         luigi.fall_package()
                         boss.fall_luigi()
                 elif pkg.belt == 1 and pkg.x >= 162:
-                    if self.collide(pkg, mario):
+                    if Collisions.collide(pkg, mario):
                         pkg.put_belt(2, 156, 50)
                         score.pkg_delivered()
                     else:
@@ -38,7 +37,7 @@ class Collisions:
                         mario.fall_package()
                         boss.fall_mario()
                 elif pkg.belt == 2 and pkg.x <= 80:
-                    if self.collide(pkg, luigi):
+                    if Collisions.collide(pkg, luigi):
                         pkg.put_belt(3, 84, 34)
                         score.pkg_delivered()
                     else:
@@ -46,7 +45,7 @@ class Collisions:
                         luigi.fall_package()
                         boss.fall_luigi()
                 elif pkg.belt == 3 and pkg.x >= 162:
-                    if self.collide(pkg, mario):
+                    if Collisions.collide(pkg, mario):
                         pkg.put_belt(4, 156, 18)
                         score.pkg_delivered()
                     else:
@@ -55,7 +54,7 @@ class Collisions:
                         boss.fall_mario()
 
                 elif pkg.belt == 4 and pkg.x <= 80:
-                    if self.collide(pkg, luigi):
+                    if Collisions.collide(pkg, luigi):
                         score.pkg_delivered()
                         pkg.at_truck = True
                         truck.add_package()
